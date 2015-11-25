54b1ef320b4373ffb0fe32df557cfe08acdc47d6
46066294p <46066294p@iespoblenou.org>
2015-11-25 20:38:05 +0100

    8e commit

---
+++ app/src/main/java/poblenou/rottentomatoesclient/MainActivityFragment.java
@@ -0,0 +1,70 @@
+package poblenou.rottentomatoesclient;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_pelis_fragment, menu);
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //View lvPelis = rootView.findViewById(R.id.lvPelis);
+        ListView lvPelis = (ListView) rootView.findViewById(R.id.lvPelis);
+
+        String[] data = {
+                "Los 400 golpes",
+                "El odio",
+                "El padrino",
+                "El padrino. Parte II",
+                "Ocurrió cerca de su casa",
+                "Infiltrados",
+                "Umberto D."
+                         };
+
+        items = new ArrayList<>(Arrays.asList(data));
+
+        adapter = new ArrayAdapter<>(
+                 getContext(),
+                 R.layout.lvpelis_row,
+                 R.id.tvPeli,
+                 items);
+
+        lvPelis.setAdapter(adapter);
+        return rootView;
+    }
+}
